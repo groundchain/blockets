a286116af0a5f0bf60b54c44b942088a676a52ef
Andres <andres.rovira@gmail.com>
2024-07-15 22:14:33 -0600

    Initial hardhat infra

---
+++ .github/pull_request_template.md
@@ -0,0 +1,22 @@
+# Background
+
+_List the things that lead to these changes_
+
+# Solution
+
+_Specify the decision that was made to perform these changes_
+
+# Main changes
+
+_Guide the reviewer through the list of changes this PR contains that address the particular issue trying to solve_
+
+# Additional changes
+
+_Optional, if you make changes that are not part of the solution, but improve the code base in any way (documentation, library upgrades, reduce tech debt, refactoring), you can list them here_
+
+# Readyness checks
+
+- [ ] Code is clean
+- [ ] No commented code
+- [ ] Test cases added if required
+- [ ] Passing build
---
+++ hardhat.config.ts
@@ -0,0 +1,42 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.1",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    hardhat: {},
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: process.env.PRIVATE_KEY !== undefined 
+        ? [process.env.PRIVATE_KEY.startsWith('0x') ? process.env.PRIVATE_KEY : `0x${process.env.PRIVATE_KEY}`]
+        : [],
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD"
+  },
+  typechain: {
+    outDir: "typechain-types",
+    target: "ethers-v6"
+  },
+};
+
+export default config;
