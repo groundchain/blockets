20933b88c6de661492a7c3cb2094c804bf64a060
Andres Rov <andres.rovira@gmail.com>
2024-07-19 18:39:02 -0600

    feat: Enhance blocket contract (#8)

--- contracts/Blocket.sol
+++ contracts/Blocket.sol
@@ -3,21 +3,37 @@ pragma solidity ^0.8.1;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 
-contract Blocket is ERC721, Ownable {
+contract Blocket is ERC721, Ownable, Pausable {
     uint256 public nextTokenId;
-    address public admin;
+    string private _baseTokenURI;
 
-    constructor() ERC721('Blocket', 'BKT') {
-        admin = msg.sender;
+    event Minted(address indexed to, uint256 indexed tokenId);
+
+    constructor() ERC721("Blocket", "BKT") {
+        _baseTokenURI = "https://yourdomain.com/metadata/";
     }
 
-    function mint(address to) external onlyOwner {
+    function mint(address to) external onlyOwner whenNotPaused {
         _safeMint(to, nextTokenId);
+        emit Minted(to, nextTokenId);
         nextTokenId++;
     }
 
     function _baseURI() internal view virtual override returns (string memory) {
-        return "https://yourdomain.com/metadata/";
+        return _baseTokenURI;
+    }
+
+    function setBaseURI(string memory baseURI) external onlyOwner {
+        _baseTokenURI = baseURI;
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
     }
 }
